69a3d4429e7282cd9feeac2833dae7c84b626aff
Chittaranjan Panda <19926216+pandaind@users.noreply.github.com>
2022-11-27 15:44:37 +0530

    Added solution - LeetHub

---
+++ Stack Permutations - GFG/stack-permutations.java
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+        
+        return a;
+    }
+    
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+    
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int N; 
+            N = Integer.parseInt(br.readLine());
+            
+            
+            int[] A = IntArray.input(br, N);
+            
+            
+            int[] B = IntArray.input(br, N);
+            
+            Solution obj = new Solution();
+            int res = obj.isStackPermutation(N, A, B);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static int isStackPermutation(int n, int[] ip, int[] op) {
+        Stack<Integer> stack = new Stack<>();
+        int oi=0;
+        if(ip.length != op.length){
+            return 0;
+        }
+        // push from ip[] ii++ to stack and peek stack and compare op[]  if equal pop oi++
+        
+        for(int ii=0; ii<ip.length; ii++){
+            stack.push(ip[ii]);
+            while(stack.size()>0 &&stack.peek()==op[oi]){
+                stack.pop();
+                oi++;
+            }
+        }
+        return oi==n?1:0;
+        
+    }
+}
+            
